fe7782ad92dcefea87d17da5ff4c49dbbfa1e878
Santiago E <santy.e@gmail.com>
2023-12-05 23:56:23 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,33 +1,68 @@
-const express = require('express');
-const mysql = require('mysql');
+// server.js
 
-const app = express();
-const port = process.env.PORT || 5000;
+require('dotenv').config(); // Load environment variables
+const express = require('express'); // Importing Express framework
+const morgan = require('morgan'); // Importing Morgan for HTTP request logging
+const cors = require('cors');
 
-// Create a MySQL connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'bemexuser',
-  password: '9001',
-  database: 'bemexdb',
-});
+const pool = require('./models/database'); // Import the database pool
+const requirementRoutes = require('./routes/requirementRoutes'); // Import requirement routes
+const studyRoutes = require('./routes/studyRoutes'); // Import study routes
+const examRoutes = require('./routes/examRoutes'); // Import exam routes
 
-// Connect to MySQL
-db.connect((err) => {
+/**
+ * Configures and starts an Express server.
+ */
+const app = express(); // Creating an instance of Express
+const port = process.env.PORT || 5000; // Setting the server port from environment variables or default to 5000
+
+// Middlewares
+app.use(morgan('dev')); // Morgan middleware for logging HTTP requests
+app.use(express.json()); // Middleware to parse JSON bodies
+
+// Test database connection on start-up
+pool.getConnection((err, connection) => {
   if (err) {
-    console.error('MySQL connection error: ' + err.stack);
+    console.error('Database connection error:', err.stack);
     return;
   }
   console.log('Connected to MySQL database');
+  connection.release(); // Release the database connection back to the pool
 });
 
-// Define your API endpoints here
+// Configuración de CORS
+const corsOptions = {
+  origin: 'http://localhost:5000',
+  methods: 'GET,PUT,POST,DELETE',
+};
 
+app.use(cors(corsOptions));
+
+// Routes
+app.use('/api', requirementRoutes); // Use requirement routes with '/api' prefix
+app.use('/api', studyRoutes); // Use study routes with '/api' prefix
+app.use('/api', examRoutes); // Use exam routes with '/api' prefix
+
+// Basic route for the homepage
 app.get('/', (req, res) => {
   res.send('Hello from your server!');
 });
 
-// Listen on the specified port
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+// Handle server shutdown and cleanup
+process.on('SIGINT', () => {
+  pool.end(err => {
+    if (err) console.error('Error closing the connection pool', err);
+    process.exit(err ? 1 : 0);
+  });
 });
